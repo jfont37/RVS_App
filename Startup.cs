c9b041c54b3cd34825b198e355d475548bf24b4f
jeffrey fontenot <jfontenot@live.com>
2021-12-28 16:56:48 -0600

    Adding more stuff

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,8 @@ namespace RVS_App
             services.AddTransient<SiteService>();
             services.AddTransient<StateService>();
             services.AddTransient<BuildingService>();
+            services.AddTransient<RoomService>();
+            services.AddTransient<RoomTypeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
